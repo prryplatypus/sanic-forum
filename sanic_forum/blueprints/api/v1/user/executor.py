f95e9c5385cd03a2fb6683041b200a54764c6e91
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2022-06-29 21:47:27 +0200

    Add ability to create user

--- sanic_forum/blueprints/api/v1/user/executor.py
+++ sanic_forum/blueprints/api/v1/user/executor.py
@@ -7,5 +7,11 @@ from sanic_forum.database.models import User
 
 @register
 class UserExecutor(PostgresExecutor):
+    async def insert_user(self, username: str) -> User:
+        ...
+
     async def select_all_users(self, limit: int, offset: int) -> List[User]:
+        ...
+
+    async def select_user_by_username(self, username: str) -> User:
         ...
